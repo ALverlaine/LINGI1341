3d918bf1a4cdbdc9d95071e1f6dedfc615b68ee9
jdoutreloux <justine.doutreloux@hotmail.be>
2016-10-20 18:43:46 +0200

    modif de test.c et du makefile

--- project1/tests/test.c
+++ project1/tests/test.c
@@ -2,28 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <CUnit/Basic.h>
-#include <CUnit/CUnit.h>
 #include <string.h>
 
-#include <packet_interface.h>
+#include "packet_interface.h"
 //#include <create_socket.h> ?
 
 
-/*	Initialisation de la suite de tests
- *
- */
-int initSuite()
-{
-	
-}
-
-/* Clean up de la suite de tests
- *
- */
-int cleanSuite()
-{
-	
-}
+///*	Initialisation de la suite de tests
+// *
+// */
+//int initSuite()
+//{
+//	
+//}
+//
+///* Clean up de la suite de tests
+// *
+// */
+//int cleanSuite()
+//{
+//	
+//}
 
 
 /* Test de pkt_new()
@@ -31,31 +30,31 @@ int cleanSuite()
  */
 void testPkt_new()
 {
-	pkt *p = pkt_new();
+	pkt_t *p = pkt_new();
 	CU_ASSERT_PTR_NOT_EQUAL(p, NULL);
 	pkt_del(p);
 }
 
-/* Test de pkt_del()
- *
- */
-void testPkt_del()
-{
-	pkt *p = pkt_new();
-	pkt_del(p);
-	CU_ASSERT_PTR_EQUAL(p, NULL);
-}
+///* Test de pkt_del()
+// *
+// */
+//void testPkt_del()
+//{
+//	pkt_t *p = pkt_new();
+//	pkt_del(p);
+//	CU_ASSERT_PTR_EQUAL(p, NULL);
+//}
 
 /* Test de pkt_decode().
  *
  */
 void testPkt_decode()
 {
-	pkt *p = pkt_new();
-	pkt *p1 = pkt_new();
-	pkt *p2 = pkt_new();
-	pkt *p3 = pkt_new();
-	pkt *p4 = pkt_new();
+	pkt_t *p = pkt_new();
+	pkt_t *p1 = pkt_new();
+	pkt_t *p2 = pkt_new();
+	pkt_t *p3 = pkt_new();
+	pkt_t *p4 = pkt_new();
 	
 	char *c = "test";
 	pkt_set_type(p, (const ptypes_t) PTYPE_DATA);
@@ -92,7 +91,7 @@ void testPkt_decode()
 	pkt_set_crc(p2, 4);
 	pkt_set_payload(p2, c, 4);
 	length = 50;
-	result = pkt_encode(p2, buf, &length)
+	result = pkt_encode(p2, buf, &length);
 	result = pkt_decode(buf, length, p1);
 	CU_ASSERT_EQUAL(result, E_TYPE);
 	
@@ -103,7 +102,7 @@ void testPkt_decode()
 	pkt_set_crc(p3, 4);
 	pkt_set_payload(p3, c, 4);
 	length = 50;
-	result = pkt_encode(p3, buf, &length)
+	result = pkt_encode(p3, buf, &length);
 	result = pkt_decode(buf, length, p1);
 	CU_ASSERT_EQUAL(result, E_LENGTH);
 	
@@ -114,7 +113,7 @@ void testPkt_decode()
 	pkt_set_crc(p4, 3);
 	pkt_set_payload(p4, c, 4);
 	length = 50;
-	result = pkt_encode(p4, buf, &length)
+	result = pkt_encode(p4, buf, &length);
 	result = pkt_decode(buf, length, p1);
 	CU_ASSERT_EQUAL(result, E_CRC);
 	
@@ -130,7 +129,7 @@ void testPkt_decode()
  */
 void testPkt_encode()
 {
-	pkt *p = pkt_new();
+	pkt_t *p = pkt_new();
 	char *c = "test";
 	pkt_set_type(p, (const ptypes_t) PTYPE_DATA);
 	pkt_set_window(p, 1);
@@ -146,13 +145,13 @@ void testPkt_encode()
 	CU_ASSERT_EQUAL(result, PKT_OK);
 	
 	size_t length1 = 10;
-	result = pkt_encode(p, buf, length1);
+	result = pkt_encode(p, buf, &length1);
 	CU_ASSERT_EQUAL(result, E_NOMEM);
 	
 	char *c1 = "test2";
 	pkt_set_payload(p, c1, 5);
 	length = 25;
-	result = pkt_encode(p, buf, length);
+	result = pkt_encode(p, buf, &length);
 	CU_ASSERT_EQUAL(length, 16); //verifier la valeur
 	
 	pkt_del(p);
@@ -164,7 +163,7 @@ void testPkt_encode()
  */
 void testPkt_getsSets()
 {
-	pkt *p = pkt_new();
+	pkt_t *p = pkt_new();
 	char *c = "tmp";
 	pkt_set_type(p, (const ptypes_t) PTYPE_DATA);
 	pkt_set_window(p, 1);
@@ -186,7 +185,7 @@ void testPkt_getsSets()
 
 
 
-int main(int argc,const char *argv[])
+int main()
 {
 	CU_pSuite pSuite = NULL;
 	
@@ -195,18 +194,18 @@ int main(int argc,const char *argv[])
 		return CU_get_error();
 	}
 	
-	pSuite = CU_add_suite("Suite de tests pour packet_implem.c", initSuite, cleanSuite);
+	pSuite = CU_add_suite("Suite de tests pour packet_implem.c", NULL, NULL);
 	if(NULL == pSuite)
 	{
 		CU_cleanup_registry();
 		return CU_get_error();
 	}
 	
-	if((NULL == CU_add_test(pSuite,"test de la première utilisation de mymalloc",testFirstMalloc)) ||
-	   (NULL == CU_add_test(pSuite, "test de la deuxième et troisième utilisation de mymalloc", testMallocMoreThanOnce)) ||
-	   (NULL == CU_add_test(pSuite,"test de free", testFree)) ||
-	   (NULL == CU_add_test(pSuite, "test de l'optimisation de malloc", testMallocGoodPlace)) ||
-	   (NULL == CU_add_test(pSuite, "test de calloc", testCalloc))
+	if((NULL == CU_add_test(pSuite,"test de l'initiation d'un packet",testPkt_new)) ||
+	   //(NULL == CU_add_test(pSuite, "test de pkt_del", testPkt_del)) ||
+	   (NULL == CU_add_test(pSuite,"test des get et set", testPkt_getsSets)) ||
+	   (NULL == CU_add_test(pSuite, "test de encode", testPkt_encode)) ||
+	   (NULL == CU_add_test(pSuite, "test de decode", testPkt_decode))
 	   )
 	{
 		CU_cleanup_registry();
