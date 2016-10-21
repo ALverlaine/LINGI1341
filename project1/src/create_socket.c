9210890bdad5c7abc92500ef96bc50ae808c864c
jdoutreloux <justine.doutreloux@hotmail.be>
2016-10-21 15:00:28 +0200

    f

--- project1/src/create_socket.c
+++ project1/src/create_socket.c
@@ -26,10 +26,6 @@ int create_socket(struct sockaddr_in6 *source_addr, int src_port, struct sockadd
         if(src_port > 0 ){
             source_addr->sin6_port=htons(src_port);
         }
-		else
-		{
-			return -1;
-		}
 			
         if(bind(sfd,(struct sockaddr *)source_addr,sizeof(struct sockaddr_in6)) == -1)
         {
@@ -37,20 +33,12 @@ int create_socket(struct sockaddr_in6 *source_addr, int src_port, struct sockadd
             return -1;
         }
     }
-	else
-	{
-		return -1;
-	}
 		
     if(dest_addr!=NULL)//cannot be null
     {
         if(dst_port>0){
             dest_addr->sin6_port=htons(dst_port);
         }
-		else
-		{
-			return -1;
-		}
 			
         if(connect(sfd,(struct sockaddr *)dest_addr,sizeof(struct sockaddr_in6))==-1)
         {
@@ -58,10 +46,6 @@ int create_socket(struct sockaddr_in6 *source_addr, int src_port, struct sockadd
             return -1;
         }
     }
-	else
-	{
-		return -1;
-	}
 		
     return sfd;//succeds
 }
